8756c45a510bb9fe33d39612ddcde2b7420adbd1
Kenley Rodriguez <kenleyrodriguez12@gmail.com>
2023-04-06 00:06:28 -0400

    Facelift

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import profile from "./assets/RODRIGUEZ_KENLEY.jpg";
+import memoji from "./assets/Memoji.png";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { Button, Carousel } from "react-bootstrap";
 import Sidebar from "./components/Sidebar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -35,108 +38,116 @@ function App() {
     handleImageClick();
   };
 
-  const handleTextSlide = (index) => {
-    if (textSlide) {
-      setTextSlide(0);
-    } else {
-      setTextSlide(1);
-    }
-  };
+  // const observer = new IntersectionObserver((entries) => {
+  //   entries.forEach((entry) => {
+  //     console.log(entry);
+  //     if (entry.isIntersecting) {
+  //       entry.target.classList.add("show");
+  //     } else {
+  //       entry.target.classList.remove("show");
+  //     }
+  //   });
+  // });
+
+  // const hiddenLeftElements = document.querySelectorAll(".hiddenLeft");
+  // const hiddenRightElements = document.querySelectorAll(".hiddenRight");
+
+  // hiddenLeftElements.forEach((el) => observer.observe(el));
+  // hiddenRightElements.forEach((el) => observer.observe(el));
 
   useEffect(() => {
+    document.body.style.backgroundColor = "black";
+    Aos.init({ duration: 2000 });
+    // document.body.style.overscrollBehaviorY = 'contain';
+    // document.documentElement.style.overflowY = 'scroll';
     // setTextSlide(0);
     // handleTextSlide();
   }, []);
 
   return (
-    <div className="Container">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100vh",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            height: "100%",
-          }}
-        >
-          <div
-            style={{
-              width: "50%",
-              padding: "8rem",
-              alignContent: "center",
-              alignSelf: "center",
-              justifyContent: "center",
-            }}
-          >
-            <h1>Hi!<br/> My name is Kenley Rodriuez</h1>
-            <div style={{ alignContent: 'center', alignItems: 'center', textAlign:'center', width: 300, }}>
-              <h2 style={{alignContent: 'center', fontWeight: 'bold', backgroundColor: 'yellow'}}>Software Engineer</h2>
+    <body>
+      <div className="Container">
+        <section>
+            <div
+              style={{
+                padding: "5rem",
+                display: "flex",
+                height: "100%",
+              }}
+            >
+              <div
+                data-aos="fade-right"
+                className="hiddenLeft"
+                style={{
+                  width: "50%",
+                  padding: "8rem",
+                  alignContent: "center",
+                  alignSelf: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <h1 style={{ fontSize: 50 }} className="white-text">
+                  Hi!
+                  <br /> My name is Kenley Rodriuez
+                </h1>
+                <div
+                  style={{
+                    alignContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    width: 300,
+                  }}
+                >
+                  <h2
+                    style={{
+                      alignContent: "center",
+                      fontWeight: "bold",
+                      backgroundColor: "orange",
+                    }}
+                  >
+                    Software Engineer
+                  </h2>
+                </div>
+                <p className="white-text">
+                  I'm a full-stack developer with a passion for building
+                  scalable and user-friendly web applications. I have experience
+                  using technologies such as React.js, Node.js, and MongoDB.
+                </p>
+              </div>
+              <div
+                data-aos="fade-left"
+                className="hiddenRight"
+                style={{
+                  width: "50%",
+                  bottom: 10,
+                  textAlign: "center",
+                  margin: "auto",
+                }}
+              >
+                <img
+                  style={{
+                    // width: "50%",
+                    // height: "100%",
+                    backgroundPosition: "center",
+                  }}
+                  src={memoji}
+                  className="App-logo"
+                  alt="profile"
+                />
+              </div>
             </div>
-            <p>
-              I'm a full-stack developer with a passion for building scalable
-              and user-friendly web applications. I have experience using
-              technologies such as React.js, Node.js, and MongoDB.
-            </p>
+        </section>
+
+        <section>
+          <div>
+            <h1 className="white-text">Hello</h1>
           </div>
-          <img
-            style={{
-              width: "50%",
-              height: "100%",
-              backgroundImage: "url(./image.jpg)",
-              backgroundPosition: "center",
-            }}
-            src={profile}
-            className="App-logo"
-            alt="profile"
-          />
-        </div>
+        </section>
+        {/* <section></section> */}
+
+        {/* <Sidebar handleClick={handleMultipleClick} /> */}
       </div>
-
-      <main className="Main" ref={(el) => (sectionsRef.current[1] = el)}>
-        <h2
-          style={{ margin: "0 0 20px 0", fontWeight: "bold", color: "white" }}
-        >
-          Projects
-        </h2>
-
-        {currentPage === "projects" && (
-          <section className="Projects-section">
-            <h2>Projects</h2>
-            <ul>
-              <li>Project 1</li>
-              <li>Project 2</li>
-              <li>Project 3</li>
-            </ul>
-          </section>
-        )}
-
-        {currentPage === "contact" && (
-          <section className="Contact-section">
-            <h2>Contact</h2>
-            <form className="Contact-form">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" />
-
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" />
-
-              <label htmlFor="message">Message:</label>
-              <textarea id="message" name="message"></textarea>
-
-              <button type="submit" className="Submit-button">
-                Submit
-              </button>
-            </form>
-          </section>
-        )}
-      </main>
-
-      <Sidebar handleClick={handleMultipleClick} />
-    </div>
+    </body>
   );
 }
 export default App;
